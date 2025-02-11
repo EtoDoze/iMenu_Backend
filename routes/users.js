0d93bebd987607715d75a2ed9eac83adb174d6d7
Renanlucass <renanlucas0014@gmail.com>
2025-02-11 15:45:00 -0300

    a

--- routes/users.js
+++ routes/users.js
@@ -50,51 +50,37 @@ userRouter.post('/create', async (req, res) => {
 });
 
 
-userRouter.post('/login', async (req, res) => {
-    try {
-        const { senha, email } = req.body;
+authRouter.post('/login', async (req, res) => {
 
-        // Verificar se os campos obrigatórios foram passados
-        if (!email || !senha) {
-            return res.status(400).json({ message: "Email e senha são obrigatórios" });
-        }
-
-        // Buscar o usuário no banco de dados
-        const finduser = await prisma.user.findUnique({
-            where: { email }
-        });
-
-        if (!finduser) {
-            return res.status(404).json({ message: "Usuário não encontrado" });
-        }
-
-        // Verificar se a senha está correta
-        const passwordMatch = await bcrypt.compare(senha, finduser.password);
-
-        if (!passwordMatch) {
-            return res.status(401).json({ message: "Senha incorreta. Tente novamente." });
-        }
-
-        // Gerar o token JWT
-        const token = jwt.sign({ userId: finduser.id }, process.env.SECRET_KEY, { expiresIn: "1h" });
-
-
-        // Retornar o token para o cliente
-        return res.status(200).json({
-            message: "Usuário logado com sucesso",
-            user: { id: finduser.id, email: finduser.email },
-            token
-        });
-
-    } catch (err) {
-        console.log("Erro ao logar com o usuário:", err); // Detalhes do erro
-        return res.status(500).json({
-            message: "Erro interno ao tentar logar",
-            error: err.message,  // Aqui retornamos o erro detalhado
-            stack: err.stack     // E o stack trace para mais detalhes
-        });
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    const user = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    })
+  
+    if (!user) {
+      console.log('email não cadastrado: ', email)
+      res.status(400).send('User or password invalid')
     }
-});
+  
+  
+    const passwordIsValid = bcrypt.compareSync(password, user.password)
+  
+    if (passwordIsValid) {
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+      delete user.password
+      res.send({
+        user,
+        token
+      })
+    } else {
+      res.status(400).send('User or password invalid')
+    }
+  
+  })
 
 
 
