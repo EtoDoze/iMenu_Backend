b087cc5d7ef64f6a340cb50611d895159974de7c
Eto12 <reverso12yt@gmail.com>
2025-06-15 15:36:32 -0300

    novo database

--- API/email.js
+++ API/email.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "imenucompany12@gmail.com", // Substitua pelo seu e-mail
-    pass: "jrpr mzsa pnyh nrff", // Substitua pela sua senha ou senha de app
+    pass: "Imenu123IFPI", // Substitua pela sua senha ou senha de app
   },
 });
 
