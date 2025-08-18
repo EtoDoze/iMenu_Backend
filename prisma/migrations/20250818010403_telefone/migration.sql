cc1d5e8f54f4e59de864acd8486bc8eb886af0e0
Eto12 <reverso12yt@gmail.com>
2025-08-17 22:04:18 -0300

    phone

---
+++ prisma/migrations/20250818010403_telefone/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "cidadeId" INTEGER,
+ADD COLUMN     "cidadeNome" TEXT,
+ADD COLUMN     "estadoId" INTEGER,
+ADD COLUMN     "estadoNome" TEXT,
+ADD COLUMN     "telefone" TEXT;
